f8519d6cc552ff5dba8b4334266af2cc1fed45f0
Lucas <regazzi58@gmail.com>
2021-01-10 23:51:45 -0500

    added portfolio item :nail_care:

---
+++ scripts/index.js
@@ -0,0 +1,13 @@
+const placeholder = {};
+
+placeholder.portfolioToggle = () => {
+    
+}
+
+placeholder.init = () => {
+    placeholder.portfolioToggle();
+}
+
+$(function () {
+    placeholder.init();
+});
